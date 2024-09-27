ecbb930bf22958aaf8a5b2c56b3b9f631a78e4f3
TacShepherd <ShepherdTA@svgs.k12.va.us>
2024-09-27 10:04:23 -0400

    users table 9/27/24

--- src/main/java/com/svgs/Main.java
+++ src/main/java/com/svgs/Main.java
@@ -1,10 +1,63 @@
 package com.svgs;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Scanner;
 
 public class Main {
+    private static Connection conn;
+    private static Statement state;
     public static void main(String[] args) {
-        String url = "jdbc:sqlite:./src/main/resources/chinook.db";
+        createDB();
+        menu();
+    }
 
+    public static void createDB() {
+        String url = "jdbc:sqlite:./src/main/resources/users.db";
+        try{
+            conn = DriverManager.getConnection(url);
+            state = conn.createStatement();
+            String query = "CREATE TABLE users(userId TEXT, password TEXT, role TEXT)";
+            state.executeUpdate(query);
+        } catch (SQLException e) {
+            System.out.println("You Suck");
+            System.out.println(e);
+        }
 
     }
+
+    public static void menu() {
+        System.out.println("1. Create User")
+        Scanner input = new Scanner(System.in);
+        String choice = input.nextLine();
+        if (choice.equals("1")) {
+            System.out.print("Username: ");
+            String username = input.nextLine();
+            System.out.print("Password: ");
+            String password = input.nextLine();
+            System.out.print("Role: ");
+            String role = input.nextLine();
+        }
+    }
+
+    public static void createUser(String u, String p, String r) {
+        
+    }
 }
+        /*try {
+            Connection conn = DriverManager.getConnection(url);
+            Statement state = conn.createStatement();
+
+            String query = "UPDATE FirstName='Garfield',LastName='Andrew' WHERE FirstName='Andrew' AND LastName = 'Adams'";
+            state.executeUpdate(query);
+
+            query = "SELECT * FROM employees";
+            state.executeUpdate(query);
+
+            
+        } catch(Exception e) {
+            System.out.println(e);
+        }*/
